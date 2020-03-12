c3734d7e07c24d3295790f5147b319d5c3cd62dc
Felipe Machado <felipemmachado28@gmail.com>
2020-03-12 18:12:10 -0300

    Criando o menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './Views/login/login.component';
 import { ContainerComponent } from './Views/container/container.component';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MenuComponent } from './Views/menu/menu.component';
 
 @NgModule({
   declarations: [
     ContainerComponent,
     LoginComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     MatSliderModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatIconModule,
     BrowserAnimationsModule,
 
   ],
